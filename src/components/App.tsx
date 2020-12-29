9152dcd012cd4038ffe6065f560fd37661b31a63
Khalid Charaa <k.charaa@gmail.com>
2020-12-29 13:14:38 +0100

    initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+interface AppProps {}
+
+function App({}: AppProps) {
+    return (
+        <div className="App">
+            APP
+        </div>
+    );
+}
+
+export default App;
